c4f352bf19384a205aa7f58b7ba909938d0b107f
Ryan Echols <rkechols21@gmail.com>
2022-12-01 02:32:59 -0700

    update template

--- template.py
+++ template.py
@@ -3,8 +3,8 @@ from typing import List
 
 from constants import INPUTS_DIR, UTF_8
 
-
 INPUT_PATH = Path(INPUTS_DIR) / "day-00.txt"
+# INPUT_PATH = Path(INPUTS_DIR) / "example.txt"
 
 
 def main(lines: List[str]):
