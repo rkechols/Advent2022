3c8d7e2a805aa4f9d9a305af34fbba6607a2b5b1
Ryan Echols <rkechols21@gmail.com>
2022-12-02 22:15:02 -0700

    day 3 solved

---
+++ day_03.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from typing import List
+
+from constants import INPUTS_DIR, UTF_8
+
+INPUT_PATH = Path(INPUTS_DIR) / "day-03.txt"
+# INPUT_PATH = Path(INPUTS_DIR) / "example.txt"
+
+
+def priority(s: str) -> int:
+    base_score = 1 + ord(s.lower()) - ord("a")
+    if s == s.upper():
+        base_score += 26
+    return base_score
+
+
+def main1(lines: List[str]) -> int:
+    total = 0
+    for line in lines:
+        line = line.strip()
+        half = len(line) // 2
+        left = line[:half]
+        right = line[half:]
+        overlap: str = next(iter(set(left) & set(right)))
+        total += priority(overlap)
+    return total
+
+
+def main2(lines: List[str]) -> int:
+    total = 0
+    for i in range(0, len(lines), 3):
+        group = [line.strip() for line in lines[i:(i + 3)]]
+        overlap: str = next(iter(set(group[0]) & set(group[1]) & set(group[2])))
+        total += priority(overlap)
+    return total
+
+
+if __name__ == "__main__":
+    with open(INPUT_PATH, "r", encoding=UTF_8) as f:
+        lines_ = f.readlines()
+    ans = main1(lines_)
+    print("part 1:", ans)
+    ans = main2(lines_)
+    print("part 2:", ans)
