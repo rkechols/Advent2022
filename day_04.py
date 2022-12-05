30c90c9ae2c50b0a0c78b1e9664ba0cf13713016
Ryan Echols <rkechols21@gmail.com>
2022-12-04 23:14:02 -0700

    day 4 - structured

--- day_04.py
+++ day_04.py
@@ -1,6 +1,6 @@
 import re
 from pathlib import Path
-from typing import List
+from typing import List, Tuple, cast
 
 from constants import INPUTS_DIR, UTF_8
 
@@ -11,22 +11,28 @@ INPUT_PATH = Path(INPUTS_DIR) / "day-04.txt"
 RANGE_PAIR_PARSE_RE = re.compile(r"(\d+)-(\d+),(\d+)-(\d+)")
 
 
-def main1(lines: List[str]):
+def parse(lines: List[str]) -> List[Tuple[int, int, int, int]]:
+    return [
+        cast(
+            Tuple[int, int, int, int],
+            tuple(map(int, RANGE_PAIR_PARSE_RE.fullmatch(line).groups()))
+        )
+        for line in lines
+    ]
+
+
+def main1(lines: List[Tuple[int, int, int, int]]):
     count = 0
-    for line in lines:
-        first_start, first_end, second_start, second_end = \
-            map(int, RANGE_PAIR_PARSE_RE.fullmatch(line).groups())
+    for first_start, first_end, second_start, second_end in lines:
         if first_start <= second_start and first_end >= second_end \
                 or second_start <= first_start and second_end >= first_end:
             count += 1
     return count
 
 
-def main2(lines: List[str]):
+def main2(lines: List[Tuple[int, int, int, int]]):
     count = 0
-    for line in lines:
-        first_start, first_end, second_start, second_end = \
-            map(int, RANGE_PAIR_PARSE_RE.fullmatch(line).groups())
+    for first_start, first_end, second_start, second_end in lines:
         if len(
                 set(range(first_start, first_end + 1))
                 & set(range(second_start, second_end + 1))
@@ -38,6 +44,7 @@ def main2(lines: List[str]):
 if __name__ == "__main__":
     with open(INPUT_PATH, "r", encoding=UTF_8) as f:
         lines_ = [line_.strip() for line_ in f.readlines()]
+    lines_ = parse(lines_)
     ans = main1(lines_)
     print("total overlap:", ans)
     ans = main2(lines_)
