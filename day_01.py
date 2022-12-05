757006abd874115df4aebed3379e74f472788478
Ryan Echols <rkechols21@gmail.com>
2022-12-04 23:03:26 -0700

    day 1 - structured

--- day_01.py
+++ day_01.py
@@ -7,17 +7,20 @@ from constants import INPUTS_DIR, UTF_8
 INPUT_PATH = Path(INPUTS_DIR) / "day-01.txt"
 
 
-def main(lines: List[str], top_k: int = 1) -> int:
-    elf_counts = []
-    this_elf = 0
-    for line in lines:
-        line = line.strip()
-        if line == "":
-            elf_counts.append(this_elf)
-            this_elf = 0
-        else:
-            this_elf += int(line)
-    elf_counts.append(this_elf)
+def parse(input_raw: str) -> List[List[int]]:
+    blocks = input_raw.strip().split("\n\n")
+    to_return = [
+        list(map(int, block.split("\n")))
+        for block in blocks
+    ]
+    return to_return
+
+
+def main(blocks: List[List[int]], top_k: int = 1) -> int:
+    elf_counts = [
+        sum(block)
+        for block in blocks
+    ]
     # get top k
     tops = []
     for count in elf_counts:
@@ -29,8 +32,9 @@ def main(lines: List[str], top_k: int = 1) -> int:
 
 if __name__ == "__main__":
     with open(INPUT_PATH, "r", encoding=UTF_8) as f:
-        lines_ = f.readlines()
-    ans = main(lines_, top_k=1)
+        input_raw_ = f.read()
+    blocks_ = parse(input_raw_)
+    ans = main(blocks_, top_k=1)
     print("1 elf:", ans)
-    ans = main(lines_, top_k=3)
+    ans = main(blocks_, top_k=3)
     print("3 elves:", ans)
