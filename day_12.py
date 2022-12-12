a3a7bec9e9de25f3a7f3baa3b27747f704e7013c
Ryan Echols <rkechols21@gmail.com>
2022-12-11 23:22:34 -0700

    day 12 - cleanup

--- day_12.py
+++ day_12.py
@@ -1,10 +1,12 @@
-import copy
 from pathlib import Path
-from typing import Tuple
+from typing import Callable, List, Tuple
 
 import numpy as np
 
 from constants import INPUTS_DIR, UTF_8
+
+StepCheck = Callable[[int, int], bool]
+EndCheck = Callable[[Tuple[int, int]], bool]
 
 INPUT_PATH = Path(INPUTS_DIR) / "day-12.txt"
 # INPUT_PATH = Path(INPUTS_DIR) / "example.txt"
@@ -18,76 +20,76 @@ def letter_to_num(c: str) -> int:
     return ord(c) - ord("a")
 
 
-def main(grid: np.ndarray, start: Tuple[int, int], end: Tuple[int, int]) -> int:
+LOWEST = letter_to_num("a")
+
+
+def parse(lines: List[str]) -> Tuple[np.ndarray, Tuple[int, int], Tuple[int, int]]:
+    n_rows = len(lines)
+    n_cols = len(lines[0])
+    grid = np.empty((n_rows, n_cols), dtype=int)
+    for i in range(n_rows):
+        for j in range(n_cols):
+            c = lines[i][j]
+            if c == START:
+                start = (i, j)
+                val = letter_to_num("a")
+            elif c == END:
+                end = (i, j)
+                val = letter_to_num("z")
+            else:
+                val = letter_to_num(c)
+            grid[i, j] = val
+    return grid, start, end
+
+
+def is_in_bounds(pos: Tuple[int, int], shape: Tuple[int, int]) -> bool:
+    return (0 <= pos[0] < shape[0]) and (0 <= pos[1] < shape[1])
+
+
+def main(
+        grid: np.ndarray,
+        start: Tuple[int, int],
+        *,
+        step_check: StepCheck,
+        end_check: EndCheck,
+) -> int:
     visited = np.zeros_like(grid, dtype=bool)
-    q = [[start]]
+    q = [start]
+    steps = 0
     while True:
         new_q = []
-        for path in q:
-            cur = path[-1]
+        steps += 1
+        for cur in q:
             this_level = grid[cur]
             for shift in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
                 next_pos = (cur[0] + shift[0], cur[1] + shift[1])
-                if next_pos[0] < 0 or next_pos[0] >= grid.shape[0] or next_pos[1] < 0 or next_pos[1] >= grid.shape[1]:
-                    continue  # out of bounds
-                if visited[next_pos]:
+                if not is_in_bounds(next_pos, grid.shape) or visited[next_pos]:
                     continue
                 next_level = grid[next_pos]
-                if next_level - this_level > 1:
+                if not step_check(this_level, next_level):
                     continue
-                if next_pos == end:
-                    return len(path)
-                new_path = copy.deepcopy(path)
+                if end_check(next_pos):
+                    return steps
                 visited[next_pos] = True
-                new_path.append(next_pos)
-                new_q.append(new_path)
-        q = new_q
-
-
-def main2(grid: np.ndarray, start: Tuple[int, int]) -> int:
-    visited = np.zeros_like(grid, dtype=bool)
-    q = [[start]]
-    while True:
-        new_q = []
-        for path in q:
-            cur = path[-1]
-            this_level = grid[cur]
-            for shift in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                next_pos = (cur[0] + shift[0], cur[1] + shift[1])
-                if next_pos[0] < 0 or next_pos[0] >= grid.shape[0] or next_pos[1] < 0 or next_pos[1] >= grid.shape[1]:
-                    continue  # out of bounds
-                if visited[next_pos]:
-                    continue
-                next_level = grid[next_pos]
-                if next_level - this_level < -1:
-                    continue
-                if next_level == 0:
-                    return len(path)
-                new_path = copy.deepcopy(path)
-                visited[next_pos] = True
-                new_path.append(next_pos)
-                new_q.append(new_path)
+                new_q.append(next_pos)
         q = new_q
 
 
 if __name__ == "__main__":
     with open(INPUT_PATH, "r", encoding=UTF_8) as f:
         lines_ = [line_.strip() for line_ in f.readlines()]
-
-    grid_ = np.empty((len(lines_), len(lines_[0])), dtype=int)
-    for i_ in range(len(lines_)):
-        for j_ in range(len(lines_[0])):
-            c_ = lines_[i_][j_]
-            if c_ == START:
-                start_ = (i_, j_)
-                val_ = letter_to_num("a")
-            elif c_ == END:
-                end_ = (i_, j_)
-                val_ = letter_to_num("z")
-            else:
-                val_ = letter_to_num(c_)
-            grid_[i_, j_] = val_
-    ans = main(grid_, start_, end_)
+    grid_, start_, end_ = parse(lines_)
+    ans = main(
+        grid_,
+        start_,
+        step_check=(lambda cur_lev, next_lev: next_lev - cur_lev <= 1),
+        end_check=(lambda pos_: pos_ == end_),
+    )
     print("part 1:", ans)
-    ans = main2(grid_, end_)
+    ans = main(
+        grid_,
+        end_,
+        step_check=(lambda cur_lev, next_lev: cur_lev - next_lev <= 1),
+        end_check=(lambda pos_: grid_[pos_] == LOWEST),
+    )
     print("part 2:", ans)
