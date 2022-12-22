43322efb4463dcd41d4af1b7f65fbeab42736cd7
Ryan Echols <rkechols21@gmail.com>
2022-12-22 11:37:14 -0600

    day 17 solved

--- day_17.py
+++ day_17.py
@@ -1,5 +1,5 @@
-from itertools import cycle
 from pathlib import Path
+from typing import Tuple
 
 import numpy as np
 
@@ -36,6 +36,7 @@ SHAPES = [
         [True, True],
     ]),
 ]
+N_SHAPES = len(SHAPES)
 BIGGEST_SHAPE_HEIGHT = max(a.shape[0] for a in SHAPES)
 
 
@@ -71,27 +72,53 @@ def bfs(canvas: np.ndarray) -> int:
     return deepest
 
 
-def main(data: str, *, n_rocks: int):
+def bool_arr_to_int(arr: np.ndarray) -> int:
+    bin_string = "".join("1" if elem else "0" for elem in np.nditer(arr))
+    return int(bin_string, 2)
+
+
+class State:
+    def __init__(self, index_instruction: int, n_landed: int, board: np.ndarray, n_trimmed: int):
+        self.index_instruction = index_instruction
+        self.n_landed = n_landed
+        self.board = board
+        self.n_trimmed = n_trimmed
+
+    def to_key(self) -> Tuple[int, int, int]:
+        return (
+            self.index_instruction,
+            self.n_landed % N_SHAPES,
+            bool_arr_to_int(self.board),
+        )
+
+
+def main(instructions: str, *, n_rocks: int):
+    n_instructions = len(instructions)
     canvas = np.zeros((BIGGEST_SHAPE_HEIGHT + BOTTOM_START_GAP, WIDTH), dtype=bool)
     highest = 0
-    n_shapes = len(SHAPES)
     n_landed = 0
-    trimmed = 0
+    n_trimmed = 0
     # first rock
-    cur_shape = SHAPES[n_landed % n_shapes]
+    cur_shape = SHAPES[n_landed % N_SHAPES]
     h_shift = LEFT_START_GAP
     bottom_edge = highest + BOTTOM_START_GAP
-    for push in cycle(data):  # infinite repeats
+    # watch for repeats
+    state_cache = {}
+    cycle_found = False
+    # start looping over instructions
+    index_instruction = 0
+    while True:  # infinite repeats of instruction list
+        instruction = instructions[index_instruction]
         # do push
-        if push == LEFT:
+        if instruction == LEFT:
             if h_shift > 0 and not has_overlap(canvas, cur_shape, bottom_edge, h_shift - 1):
                 h_shift -= 1
-        elif push == RIGHT:
+        elif instruction == RIGHT:
             if h_shift < WIDTH - cur_shape.shape[1] \
                     and not has_overlap(canvas, cur_shape, bottom_edge, h_shift + 1):
                 h_shift += 1
         else:
-            raise ValueError(f"invalid push value: {push}")
+            raise ValueError(f"invalid push value: {instruction}")
         # do fall
         if bottom_edge == 0 or has_overlap(canvas, cur_shape, bottom_edge - 1, h_shift):
             # can't fall; land where we are
@@ -103,17 +130,16 @@ def main(data: str, *, n_rocks: int):
             highest = max(highest, this_top)
             n_landed += 1
             if n_landed == n_rocks:
-                return trimmed + highest
-            # occasionally trim if possible
-            if n_landed % 10000 == 0:
-                deepest_reachable = bfs(canvas)
-                trim_index = deepest_reachable + 2
-                current_trim_size = canvas.shape[0] - trim_index
-                if current_trim_size > 0:
-                    canvas = canvas[:trim_index]
-                    bottom_edge -= current_trim_size
-                    highest -= current_trim_size
-                    trimmed += current_trim_size
+                return n_trimmed + highest
+            # trim if possible
+            deepest_reachable = bfs(canvas)
+            trim_index = deepest_reachable + 2
+            current_trim_size = canvas.shape[0] - trim_index
+            if current_trim_size > 0:
+                canvas = canvas[:trim_index]
+                bottom_edge -= current_trim_size
+                highest -= current_trim_size
+                n_trimmed += current_trim_size
             # add padding to the top if needed
             n_blank_rows = canvas.shape[0] - highest
             n_blanks_to_add = (BIGGEST_SHAPE_HEIGHT + BOTTOM_START_GAP) - n_blank_rows
@@ -122,12 +148,36 @@ def main(data: str, *, n_rocks: int):
                     np.zeros((n_blanks_to_add, WIDTH), dtype=bool),
                     canvas,
                 ])
+            # save state, check if we're in a cycle
+            if not cycle_found:
+                state = State(
+                    index_instruction=index_instruction,
+                    n_landed=n_landed,
+                    board=canvas,
+                    n_trimmed=n_trimmed,
+                )
+                state_key = state.to_key()
+                if state_key in state_cache:  # we have a cycle
+                    cycle_found = True
+                    prev_state = state_cache[state_key]
+                    # what does the cycle pattern look like
+                    cycle_start = prev_state.n_landed
+                    cycle_length = state.n_landed - prev_state.n_landed
+                    n_cycles_complete = (n_rocks - cycle_start) // cycle_length
+                    cycle_trim_diff = state.n_trimmed - prev_state.n_trimmed
+                    # skip a bunch of steps
+                    n_landed = cycle_start + n_cycles_complete * cycle_length
+                    n_trimmed = prev_state.n_trimmed + n_cycles_complete * cycle_trim_diff
+                else:  # not a repeat
+                    state_cache[state_key] = state
             # get a new rock
-            cur_shape = SHAPES[n_landed % n_shapes]
+            cur_shape = SHAPES[n_landed % N_SHAPES]
             h_shift = LEFT_START_GAP
             bottom_edge = highest + BOTTOM_START_GAP
         else:  # normal fall
             bottom_edge -= 1
+        # get ready for next instruction
+        index_instruction = (index_instruction + 1) % n_instructions
 
 
 if __name__ == "__main__":
